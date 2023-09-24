3dfa3e02f0acc496d2552944ebd8da57a30e8493
fzw <fanzhiwencc@163.com>
2023-09-24 19:52:23 +0800

    fix: test fix the nvim keymaps bug

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -6,30 +6,34 @@
 --vim.g.maplocalleader = "\"
 
 -- 设置别名，便于后续设置
-local function map(mode, lhs, rhs, opts)
-  local keys = require("lazy.core.handler").handlers.keys
-  ---@cast keys LazyKeysHandler
-  -- do not create the keymap if a lazy keys handler exists
-  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
-    opts = opts or {}
-    opts.silent = opts.silent ~= false
-    if opts.remap and not vim.g.vscode then
-      opts.remap = nil
-    end
-    vim.keymap.set(mode, lhs, rhs, opts)
-  end
-end
+-- local function map(mode, lhs, rhs, opts)
+--   local keys = require("lazy.core.handler").handlers.keys
+--   ---@cast keys LazyKeysHandler
+--   -- do not create the keymap if a lazy keys handler exists
+--   if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+--     opts = opts or {}
+--     opts.silent = opts.silent ~= false
+--     if opts.remap and not vim.g.vscode then
+--       opts.remap = nil
+--     end
+--     vim.keymap.set(mode, lhs, rhs, opts)
+--   end
+-- end
+
+local map = vim.keymap.set
+
 -- 复用参数
 local opt = { noremap = true, silent = true }
 
+-- https://neovim.io/doc/user/vimindex.html
 --- 系统剪切板的复制/粘贴（vim需要支持clipboard功能）
-map("n", "<C-A-y>", '"+y', opt)
-map("n", "<C-A-p>", '"*p', opt)
+-- map("n", "<C-A-y>", '"+y', opt) nvim已经默认复制粘贴到剪贴板了
+-- map("n", "<C-A-p>", '"*p', opt)
 ------------------- insert模式 -------------------
 -- 退出insert模式
 map("i", "jk", "<ESC>", opt)
 map("i", "<C-B>", "<LEFT>", opt)
 map("i", "<C-F>", "<RIGHT>", opt)
 ------------------- visual模式 -------------------
--- 退出insert模式
+-- 退出visual模式
 map("v", "jk", "<ESC>", opt)
